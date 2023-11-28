868c39246e57fca3578e45c452c5f29de72234aa
Aaron Bannin <aaronbannin@Aarons-MacBook-Pro.local>
2023-11-28 10:16:12 -0800

    begin looking into questions

---
+++ essential_takehome/questions/__init__.py
@@ -0,0 +1,50 @@
+base = [
+    {
+        "question": "In which month was the most products sold?",
+        "answers": ["July"],
+        "difficulty": "easy",
+        "reasoning": ""
+    },
+    # {
+    #     "question": "What were the 5 most common product colors sold in Europe in May, June, and July",
+    #     "answers": ["Orange", "Grey", "Purple", "Blue", "Red"],
+    #     "difficulty": "easy",
+    #     "reasoning": ""
+    # },
+    # {
+    #     "question": "What is the most common color amongst the different clothing types?",
+    #     "answers": [],
+    #     "difficulty": "easy",
+    #     "reasoning": ""
+    # },
+    # {
+    #     "question": "Identify top customers with high purchases and determine what is their common frequency to buy?",
+    #     "answers": [],
+    #     "difficulty": "easy",
+    #     "reasoning": ""
+    # },
+    # {
+    #     "question": "How many products does a customer browse before making a purchase on weekends and holidays vs weekdays?",
+    #     "answers": [],
+    #     "difficulty": "easy",
+    #     "reasoning": ""
+    # },
+    # {
+    #     "question": "What are the most common sizes available for the top-selling products?",
+    #     "answers": [],
+    #     "difficulty": "easy",
+    #     "reasoning": ""
+    # },
+    # {
+    #     "question": "Are there any trends in product features (texture, color, type) and the session activity (time spent, avg purchases, etc.)?",
+    #     "answers": [],
+    #     "difficulty": "easy",
+    #     "reasoning": ""
+    # },
+    # {
+    #     "question": "What is the average spend in the summer vs spring?",
+    #     "answers": [],
+    #     "difficulty": "easy",
+    #     "reasoning": ""
+    # }
+]
